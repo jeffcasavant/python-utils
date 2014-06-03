3923aab24488dfc2835b089fe9b152c3f2095ce3
Jeff Casavant <jeff.casavant@gmail.com>
2014-06-03 13:01:30 -0400

    Removed erroneous comment in example

--- pidfile/pidfile-example.py
+++ pidfile/pidfile-example.py
@@ -2,11 +2,6 @@
 
 # Example of the use of a pidfile
 
-# Notes: be sure to os.remove(pidfile) before any calls to sys.exit() that fall
-# inside the if statement; there should only be one sys.exit() without a matching
-# os.remove(pidfile), and that is in the else clause that throws an error b/c 
-# the script is currently running
-
 import sys
 
 import pidfile
