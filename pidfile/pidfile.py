2bd695dd44d03ed8f1045b5006560c5e67b2474a
Jeff Casavant <jeff.casavant@gmail.com>
2014-06-03 15:23:03 -0400

    Added more verbose errors & pidfilepath option

--- pidfile/pidfile.py
+++ pidfile/pidfile.py
@@ -8,9 +8,15 @@
 # continue_on_error
 #	Continue if we find a "zombie" pidfile; if False then sys.exit(1)
 # 	Default True
+# pidfilePath
+#	Set the path in which to store/look for pidfiles
+#	Default "/tmp/"
+
+# Jeff Casavant 06/03/2014
 
 import os
 import sys
+import pwd
 import atexit
 
 # Location of pidfile
@@ -19,8 +25,11 @@ pidfile = pidfilePath + sys.argv[0] + ".pid"
 pid = str(os.getpid())
 
 # Function to be called externally
-def use(continue_on_error=True):
-	if not os.path.exists(pidfile):
+def use(continue_on_error=True, pidfilePath="/tmp/"):
+	
+	pidfile = pidfilePath + sys.argv[0] + ".pid"
+	
+	if not os.access(pidfile):
 		_create_pidfile()
 	else:
 		if _running():
@@ -35,11 +44,20 @@ def use(continue_on_error=True):
 				sys.exit(1)
 
 def _create_pidfile():
-	pf = open(pidfile, 'w')
-	pf.write(pid)
-	pf.close()
-	# Delete pidfile on exit()
-	atexit.register(_exit)
+	try:
+		pf = open(pidfile, 'w')
+		pf.write(pid)
+		pf.close()
+		# Delete pidfile on exit()
+		atexit.register(_exit)
+	except Exception, e:
+		if os.path.exists(pidfile):
+			uid = os.stat(pidfile).st_uid
+			name = pwd.getpwuid(uid)
+			print "Could not write to %s: owned by UID %s %s" % (pidfile, uid, name)
+		else:
+			print "Could not write to %s" % pidfile
+
 
 def _running():
 	pf = open(pidfile, 'r')
